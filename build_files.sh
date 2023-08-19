89f15d4aa801485e855f7d4c94b7fb1bb06ddd34
Husni Abad <husni.abad.97@gmail.com>
2023-08-20 02:00:40 +0530

    deploy to vercel init

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,15 @@
-pip install -r requirements.txt
+# Create a virtual environment
+echo "Creating a virtual environment..."
+python3.9 -m venv venv
+source venv/bin/activate
 
-python3.9 manage.py collectstatic
+echo "Installing the latest version of pip..."
+python -m pip install --upgrade pip
+
+# Build the project
+echo "Building the project..."
+python -m pip install -r requirements.txt
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear

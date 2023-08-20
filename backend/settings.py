de1e0a6c09a844b737c55dacd07996a178ec2b8e
Husni Abad <husni.abad.97@gmail.com>
2023-08-20 16:18:48 +0530

    chech update deployment

--- backend/settings.py
+++ backend/settings.py
@@ -202,7 +202,7 @@ print("base dir",BASE_DIR)
 MEDIA_ROOT = BASE_DIR / 'static/images'
 # STATIC_ROOT = BASE_DIR / 'staticfiles_build'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static', 'frontend/build')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR,  'frontend/build', 'static'),
 
